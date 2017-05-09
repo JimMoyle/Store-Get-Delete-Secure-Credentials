36b8a6fe61ca709821d38cacf5c31d729f24340a
jimmoyle <git@jimmoyle.com>
2017-05-09 12:59:36 +0100

    JM + stopped error on reg key not existing

--- Store-Get-Delete-Secure-Credentials.ps1
+++ Store-Get-Delete-Secure-Credentials.ps1
@@ -1,4 +1,4 @@
-﻿<#	
+﻿<#
 	.NOTES
 	===========================================================================
 	 Created on:   	15/12/2016 13:43
@@ -22,7 +22,7 @@ function Add-SecureCredential
 {
 	[CmdletBinding()]
 	param (
-		
+
 		[Parameter(
 			Position = 0,
 		 	ValuefromPipelineByPropertyName = $true,
@@ -30,15 +30,15 @@ function Add-SecureCredential
 			HelpMessage = 'Type the name under which you wish to store these credentials'
 		)]
 		[System.String]$Name,
-		
+
 		[Parameter(
 			Position = 1,
 			ValuefromPipelineByPropertyName = $true,
 			Mandatory = $true
-			
+
 		)]
 		[System.String]$Username,
-		
+
 		[Parameter(
 			Position = 2,
 			ValuefromPipelineByPropertyName = $true,
@@ -46,7 +46,7 @@ function Add-SecureCredential
 			Mandatory = $true
 		)]
 		[System.String]$PlainTextPassword,
-		
+
 		[Parameter(
 				   Position = 2,
 				   ValuefromPipelineByPropertyName = $true,
@@ -55,20 +55,20 @@ function Add-SecureCredential
 		)]
 		[System.Security.SecureString]$Password
 	)
-	
-	
+
+
 	BEGIN {
 	}
-	
+
 	PROCESS {
-		
+
 		$pathRoot = "HKCU:\Software\SecureCredentials"
-		
-		if (-not (Test-Path -Path $pathRoot))
+
+		if (-not (Test-Path -Path $pathRoot -ErrorAction SilentlyContinue))
 		{
 			New-Item -Path $pathRoot -ErrorAction Stop | Out-Null
 		}
-							
+
 		switch ($PsCmdlet.ParameterSetName)
 		{
 			'SecureString' {
@@ -79,25 +79,25 @@ function Add-SecureCredential
 				$securePasswordString = $PlainTextPassword | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString
 				break
 			}
-			
+
 		}
-		
+
 		$userNameString = $Username
-		
+
 		$credPath = "$pathRoot\$Name"
-		
+
 		Write-Verbose "Storing credential $usernameString under $credPath."
-		
+
 		try
 		{
 			New-Item -Path $credPath -ErrorAction Stop | Out-Null
-			
+
 			$params = @{
 				'Path' = $credPath
 				'PropertyType' = 'String'
 				'ErrorAction' = 'Stop'
 			}
-			
+
 			New-ItemProperty @params -Name UserName -Value $userNameString | Out-Null
 			New-ItemProperty @params -Name Password -Value $securePasswordString | Out-Null
 		}
@@ -105,7 +105,7 @@ function Add-SecureCredential
 		{
 			Write-Error "Could not create Credential in $credPath."
 		}
-		
+
 	}
 	END {
 	}
@@ -125,57 +125,57 @@ function Get-SecureCredential
 		)]
 		[System.String]$Name
 	)
-	
-	
+
+
 	BEGIN {
 	}
-	
+
 	PROCESS
 	{
-		
+
 		$pathRoot = 'HKCU:\Software\SecureCredentials'
-		
+
 		if ($Name)
 		{
-			
+
 			if (-not (Test-Path -Path $pathRoot\$Name))
 			{
 				Write-Error "Credentials cannot be found at $pathRoot\$Name"
 			}
-			
+
 			$secureCredUserName = Get-ItemProperty -Path $pathRoot\$Name | Select-Object -ExpandProperty UserName
 			$secureCredPassword = Get-ItemProperty -Path $pathRoot\$Name | Select-Object -ExpandProperty Password
 			$securePassword = ConvertTo-SecureString $secureCredPassword
 			$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $secureCredUserName, $securePassword
-			
+
 			Write-Output $credential
 
 		}
 		else
 		{
 			$list = Get-ChildItem $pathRoot | Select-Object @{ n = 'Name'; e = { $_.PSChildName } }
-			
+
 			$credlist = @()
-			
+
 			foreach ($cred in $list.name)
 			{
 				$secureCredUserName = Get-ItemProperty -Path $pathRoot\$cred | Select-Object -ExpandProperty UserName
 				$secureCredPassword = Get-ItemProperty -Path $pathRoot\$cred | Select-Object -ExpandProperty Password
 				$securePassword = ConvertTo-SecureString $secureCredPassword
 				$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $secureCredUserName, $securePassword
-				
+
 				$credWithName = $credential | Select-Object -Property @{ n = 'Name';  e= { $cred } }, UserName, Password
-				
-				
+
+
 				$credlist += $credWithName
 			}
-			
+
 			Write-Output $credlist
-			
+
 		}
-		
+
 	}
-	
+
 	END {
 	}
 }
@@ -186,7 +186,7 @@ function Get-SecureCredential
 function Remove-SecureCredential
 {
 	[CmdletBinding()]
-	
+
 	param (
 		[Parameter(
 			Position = 0,
@@ -195,25 +195,25 @@ function Remove-SecureCredential
 		)]
 		[System.String]$Name
 	)
-	
+
 	BEGIN{
 	}
-	
+
 	PROCESS
 	{
-		
+
 		$pathRoot = 'HKCU:\Software\SecureCredentials'
-		
+
 		if (-not (Test-Path -Path $pathRoot\$Name))
 		{
 			Write-Error "$Name Credentials cannot be found at $pathRoot\$Name"
 			break
 		}
-		
+
 		Remove-Item -Path $pathRoot\$Name
-		
+
 	}
-	
+
 	END{
 	}
 }
